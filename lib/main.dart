be196b60141b724440ddec359ac4336500b448ea
Your Name <you@example.com>
2022-09-05 08:57:38 -0300

    adicionamos um carrossel de pesquisa rápida2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     home: HomePage(),
   ));
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -18,34 +19,33 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       drawer: Drawer(),
       appBar: AppBar(
-        title: const Text("AstroEstelar", style: TextStyle(
-          fontSize: 28,
-          letterSpacing: 3,
-          fontWeight: FontWeight.bold,
-          color: Colors.white
-        )),
+        title: const Text("AstroEstelar",
+            style: TextStyle(
+                fontSize: 28,
+                letterSpacing: 3,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
         centerTitle: true,
       ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("images/background.jpeg"),
-            fit: BoxFit.cover
-          ),
+              image: new AssetImage("images/background.jpeg"),
+              fit: BoxFit.cover),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ListTile(
               leading: Icon(Icons.verified_user, color: Colors.white),
-              trailing: Text("AstroEstelar", style: TextStyle(
-                fontSize: 26,
-                letterSpacing: 3,
-                fontWeight: FontWeight.bold,
-                color: Colors.white
-              )),
+              trailing: Text("AstroEstelar",
+                  style: TextStyle(
+                      fontSize: 26,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
             ),
-            SizedBox(height: 100),
+            SizedBox(height: 40),
             Container(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
@@ -60,9 +60,8 @@ class _HomePageState extends State<HomePage> {
                           filled: true,
                           fillColor: Colors.white,
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                            borderSide: BorderSide.none
-                          ),
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide.none),
                           hintText: "Pesquisar por planetas...",
                         ),
                       ),
@@ -71,6 +70,38 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: Card(
+                      child: Row(
+                        children: [Text("Objetos deixados \n na lua \n pelo homem")],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: Card(
+                      child: Row(
+                        children: [Text("Primeiro \n sátelite \n feito"), Icon(Icons.abc)],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: Card(
+                      child: Row(
+                        children: [Text("Composição de \n de um boraco \n negro"), Icon(Icons.abc)],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
